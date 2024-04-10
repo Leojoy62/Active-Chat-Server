1790e5a039fbb79aced0e40809b9cffbeadcc5d7
Leojoy62 <leojoy62@gmail.com>
2024-04-11 01:06:18 +0600

    cors problem

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ const io = new Server(server, {
   cors: {
     origin: "http://localhost:5173",
     methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
   },
 });
+
 io.on("connection", (socket) => {
   console.log(`New user connected ${socket.id}`);
 
