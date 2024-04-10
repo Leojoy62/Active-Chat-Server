a309821f65a3cda3d88442273c051523bcaca104
Leojoy62 <leojoy62@gmail.com>
2024-04-11 02:46:55 +0600

    cors problem

--- index.js
+++ index.js
@@ -8,13 +8,12 @@ const { Server } = require("socket.io");
 app.use(cors());
 
 const io = new Server(server, {
-  wssEngine: ["ws", "wss"],
-  transports: ["websocket", "polling"],
+  pingTimeout: 60000,
+  pingInterval: 25000,
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
   },
-  allowEIO3: true,
 });
 
 io.on("connection", (socket) => {
@@ -35,7 +34,7 @@ io.on("connection", (socket) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Hey2");
+  res.send("Hey3");
 });
 
 server.listen(3001, () => {
